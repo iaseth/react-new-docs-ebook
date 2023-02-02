03f8bd2d3d974c33466825a93e1caa44ca32b177
Ankur Seth <iankurseth@gmail.com>
2023-02-02 12:36:58 +0530

    Updated download_index.py to use repo JSONs to generate index * section -> chapter -> page

--- download_index.py
+++ download_index.py
@@ -1,7 +1,5 @@
 import json
 
-import requests
-import bs4
 
 
 def sanitize_string(s):
@@ -11,45 +9,40 @@ def sanitize_string(s):
 			s[idx] = '_'
 	return ''.join(s)
 
+def get_route_object(route):
+	path = route["path"]
+	route_object = {
+		"filename": sanitize_string("path"),
+		"path": path,
+		"title": route["title"],
+		"URL": f"https://beta.reactjs.org{path}",
+	}
+	return route_object
+
 def main():
-	parts = [
-		{"title": "Home", "url": "https://beta.reactjs.org"},
-		{"title": "Learn", "url": "https://beta.reactjs.org/learn"},
-		{"title": "API", "url": "https://beta.reactjs.org/apis/react"}
-	]
+	learn_json = json.load(open("data/sidebarLearn.json"))
+	reference_json = json.load(open("data/sidebarReference.json"))
+	routes = learn_json["routes"][0]["routes"] + reference_json["routes"][0]["routes"]
 
-	for pidx, part in enumerate(parts):
-		print(f"Creawling part [{pidx+1}/{len(parts)}]:")
-		print(f"\t-- downloading ({part['url']})")
-		response = requests.get(part['url'])
-		soup = bs4.BeautifulSoup(response.text, 'lxml')
-
-		nav = soup.find('nav', attrs={'role': 'navigation'})
-		part['sections'] = []
-		for sidx, section_li_tag in enumerate(nav.find('ul').find_all('li', recursive=False)):
-			section_a_tag = section_li_tag.find('a')
+	sections = []
+	section = None
+	for route in routes:
+		if "hasSectionHeader" in route:
 			section = {
-				"title": section_a_tag.text,
-				"url": section_a_tag['href'],
-				"filename": sanitize_string(section_a_tag['href']),
-				"pages": []
+				"sectionHeader": route["sectionHeader"],
+				"chapters": []
 			}
-			part["sections"].append(section)
+			sections.append(section)
+		else:
+			chapter = get_route_object(route)
+			chapter["pages"] = []
+			section["chapters"].append(chapter)
+			for page_route in route["routes"]:
+				page = get_route_object(page_route)
+				chapter["pages"].append(page)
 
-			page_list = section_li_tag.find('ul')
-			if not page_list: continue
-			for idx, page_li_tag in enumerate(page_list.find_all('li', recursive=False)):
-				page_a_tag = page_li_tag.find('a')
-				page = {
-					"title": page_a_tag.text,
-					"url": page_a_tag['href'],
-					"filename": sanitize_string(page_a_tag['href']),
-				}
-				section['pages'].append(page)
-
-			print(f"\t-- added {len(section['pages'])} pages to section '{section['title']}'")
-
-	jo = {"parts": parts}
+	jo = {}
+	jo["sections"] = sections
 	output_json_name = 'pages.json'
 	with open(output_json_name, 'w') as f:
 		json.dump(jo, f, indent='\t')
