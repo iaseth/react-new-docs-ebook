130abcbbf82b1a48ec987b8e4331d67e47604130
Ankur Seth <iankurseth@gmail.com>
2023-02-05 10:19:11 +0530

    Added tailwind, updated project structure

---
+++ reactapp/src/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+
+
+function App () {
+
+	return (
+		<div className="App min-h-screen bg-red-500 text-white flex">
+			<h1 className="m-auto">React docs for Kindle</h1>
+		</div>
+	);
+}
+
+export default App;
